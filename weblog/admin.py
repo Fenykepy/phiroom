9ed4f61040919b6f4561326350ac38741ed15cfa
Fenykepy <phiroom@lavilotte-rolle.fr>
2014-12-06 21:07:32 +0100

    Improve weblog design

--- weblog/admin.py
+++ weblog/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from weblog.models import Post, Tag
 
 
+
 class PostAdmin(admin.ModelAdmin):
     fields = ('title', 'description', 'source', 'tags', 'draft', 'pub_date')
     def save_model(self, request, obj, form, change):
@@ -16,3 +17,4 @@ class TagAdmin(admin.ModelAdmin):
     fields = ('name',)
 
 admin.site.register(Tag, TagAdmin)
+
