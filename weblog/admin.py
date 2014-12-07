87e571dc55cc31aee031564353b6bd0c8559e3fd
Fenykepy <phiroom@lavilotte-rolle.fr>
2014-12-07 22:12:28 +0100

    improve css and templates for weblog app

--- weblog/admin.py
+++ weblog/admin.py
@@ -6,7 +6,8 @@ from weblog.models import Post, Tag
 class PostAdmin(admin.ModelAdmin):
     fields = ('title', 'description', 'source', 'tags', 'draft', 'pub_date')
     def save_model(self, request, obj, form, change):
-        obj.author = request.user
+        if not obj.pk:
+            obj.author = request.user
         obj.save()
 
 admin.site.register(Post, PostAdmin)
