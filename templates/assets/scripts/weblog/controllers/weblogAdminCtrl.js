809af98a44b19c8b2821426e7ebc46b720343261
Fenykepy <phiroom@lavilotte-rolle.fr>
2014-12-08 20:23:55 +0100

    improve templates

--- templates/assets/scripts/weblog/controllers/weblogAdminCtrl.js
+++ templates/assets/scripts/weblog/controllers/weblogAdminCtrl.js
@@ -7,8 +7,8 @@ var weblogControllers = angular.module('weblogControllers');
 weblogControllers.controller('weblogAdminCtrl', ['$scope', '$http', 
         function($scope, $http) {
             // request actual post
-            $http.get('/weblog/api/posts/6/').success(function(data) {
-                $scope.post = data;
+            $http.get('/weblog/api/posts/6/').then (function (result) {
+                $scope.post = result.data;
             });
             $scope.edit = false;
 }]);
