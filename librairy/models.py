413f3c5d9ea8c054698637a66f8ca8d48c1bd264
Fenykepy <phiroom@lavilotte-rolle.fr>
2015-05-08 18:52:34 +0200

    add collections and directorys on librairy model

--- librairy/models.py
+++ librairy/models.py
@@ -3,6 +3,9 @@ import os
 from django.db import models
 from django.core.files.storage import FileSystemStorage
 
+from mptt.models import MPTTModel, TreeForeignKey
+
+
 from thumbnail import ThumbnailFactory
 
 
@@ -39,11 +42,11 @@ class Picture(models.Model):
             verbose_name="Importation date")
     last_update = models.DateTimeField(auto_now=True,
             verbose_name="Last update date")
-    absolute_url = models.URLField()
     sha1 = models.CharField(max_length=42, unique=True, db_index=True)
     source_file = models.ImageField(upload_to=set_picturename,
             storage=PictureFileSystemStorage()
     )
+    directory = models.ForeignKey('Directory', verbose_name="Folder")
     title = models.CharField(max_length=140, null=True, blank=True,
             verbose_name="Title")
     legend = models.TextField(null=True, blank=True,
@@ -114,7 +117,7 @@ class Label(models.Model):
     name = models.CharField(max_length=150, verbose_name="Name")
     slug = models.SlugField(max_length=150, verbose_name="Slug")
     color = models.CharField(max_length=7, default="#CB5151",
-        verbose_name="Color")
+            verbose_name="Color")
 
     def __str__(self):
         return self.name
@@ -123,6 +126,102 @@ class Label(models.Model):
 
 
     
+class Directory(MPTTModel):
+    """Table for all directorys."""
+    name = models.CharField(max_length=150, verbose_name="Name")
+    slug = models.SlugField(max_length=150, verbose_name="Slug")
+    parent = TreeForeignKey('self', null=True, blank=True,
+            related_name="Children")
 
+    def get_children_pictures(self):
+        """Returns all pictures of a directory and its sub directorys."""
+        # if dir is leaf node, return query
+        if self.is_leaf_node():
+            return self.pictures.all()
+        # if not, search descendants
+        dir_descendants = self.get_descendants(include_self=True)
+
+
+    class MPTTMeta:
+        order_insertion_by = ['name']
+
+    def __str__(self):
+        return self.name
     
+
+
+class Collection(models.Model):
+    """Table for all collections"""
+    name = models.CharField(max_length=150, verbose_name="Collection")
+    slug = models.SlugField(max_length=150, verbose_name="Slug")
+    ensemble = models.ForeignKey('CollectionsEnsemble', null=True,
+            blank=True)
+    pictures = models.ManyToManyField(Picture,
+            through='Collection_pictures', null=True, blank=True,
+            verbose_name="Pictures")
+    n_pict = models.PositiveIntegerField(default=0,
+            verbose_name="Pictures number")
+
+    @property
+    def get_sorted_pictures(self):
+        """Returns collection's pictures ordered"""
+        return [collection.picture for collection in
+                Collection_pictures.objects.filter(collection=self)
+                .order_by('order')]
+
+    class Meta:
+        unique_together = ('name', 'ensemble')
+
+    def __str__(self):
+        return self.name
+
+
+class Collection_pictures(models.Model):
+    """Through class for collection's pictures relation, add order column"""
+    collection = models.ForeignKey(Collection)
+    picture = models.ForeignKey(Picture)
+    order = models.IntegerField(default=0)
+
+    class Meta:
+        ordering = ('order',)
+
+
+
+class CollectionsEnsemble(MPTTModel):
+    """Collections ensembles table"""
+    name = models.CharField(max_length=150,
+            verbose_name="Ensemble de collection")
+    slug = models.SlugField(max_length=150, verbose_name="slug")
+    parent = models.ForeignKey('CollectionsEnsemble', null=True, blank=True)
+
+    def get_pictures(self):
+        """Return all pictures of a collection ensembles."""
+        # select ensemble descendants
+        ensemble_descendants = self.get_descendants(include_self=True)
+
+        collections_list = []
+        # for each descendant of ensemble append children collections to list
+        for descendant in ensemble_descendants:
+            collections = Collection.objects.filter(ensemble=descendant)
+            if collections:
+                for collection in collections:
+                    collections_list.append(collection)
+
+        # set Q objects list with collections_list
+        queries = [Q(collections__id=collection.id) for collection in collections_list]
+        # Take one Q object from the list
+        query = queries.pop()
+        # Or the Q object with the ones remaining in the list
+        for item in queries:
+            query |= item
+
+        # return queryset
+        return Picture.objects.filter(query)
+
+    class Meta:
+        unique_together = ('name', 'parent')
+
+    def __str__(self):
+        return self.name
+
     
