97307d09db377b6f30590db2491b3abf39121831
Fenykepy <phiroom@lavilotte-rolle.fr>
2015-05-28 18:39:27 +0200

    add search bar to librairy.

--- statics/js/librairy/controllers/librairyGridCtrl.js
+++ statics/js/librairy/controllers/librairyGridCtrl.js
@@ -7,5 +7,6 @@ var librairyControllers = angular.module('librairyControllers');
 librairyControllers.controller('librairyGridCtrl', ['$scope', 'pictures', 'phRate',
         function($scope, pictures, phRate) {
             $scope.picts = pictures.data;
+            $scope.show_filter_bar = true;
             $scope.setRate = phRate; 
 }]);
