3cee9982f501ac31f83037adb02a832f23522f76
Fenykepy <pro@lavilotte-rolle.fr>
2015-11-11 08:19:42 +0100

    upgrade to babel 6

--- index.js
+++ index.js
@@ -1,17 +1,16 @@
-import 'babel-core/polyfill'
 import React from 'react'
 import { render } from 'react-dom'
-import { Provider } from 'react-redux'
+//import { Provider } from 'react-redux'
 import App from './containers/App'
-import configureStore from './store/configureStore'
+//import configureStore from './store/configureStore'
 
-const store = configureStore();
+//const store = configureStore()
 
 
 render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
+  //<Provider store={store}>
+    <App />,
+  //</Provider>,
   document.body
 )
 
--- reducers/index.js
+++ reducers/index.js
@@ -2,7 +2,7 @@ import { combineReducers } from 'redux';
 import portfolios from './portfolios';
 
 const rootReducer = combineReducers({
-  todos
+  portfolios
 })
 
 export default rootReducer
