577e6ed584dc7c9912b4e4532817a52df8fbe7fe
Fenykepy <phiroom@lavilotte-rolle.fr>
2015-12-04 13:16:48 +0100

    add serializers for Message and Description.

---
+++ src/api/contact/serializers.py
@@ -0,0 +1,21 @@
+from rest_framework import serializers
+
+from contact.models import Description, Message
+
+
+class DescriptionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Description
+        fields = ('title', 'source', 'content', 'author')
+        read_only_fields = ('author', 'content')
+
+
+class MessageSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Message
+        fields = ('name', 'user', 'mail', 'website',
+                  'subject', 'message', 'forward',
+                  'date', 'ip'
+        )
+        read_only_fields = ('user', 'date', 'ip')
+    
