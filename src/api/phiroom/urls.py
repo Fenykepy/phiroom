63c3cd258404d864e70a2c09f04991fbb60e9f0c
Fenykepy <pro@lavilotte-rolle.fr>
2015-12-05 21:56:13 +0100

    upgrade to delete warnings with strings in urlpatterns.

--- src/api/phiroom/urls.py
+++ src/api/phiroom/urls.py
@@ -4,6 +4,7 @@ from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.conf.urls.static import static
 from django.contrib import admin
 
+from rest_framework_jwt.views import refresh_jwt_token, obtain_jwt_token, verify_jwt_token
 from phiroom.views import api_root
 
 
@@ -19,11 +20,11 @@ urlpatterns = patterns('',
     url(r'^api/librairy/', include('librairy.urls')), # librairy API
     url(r'^api/users/', include('user.urls')), # users API
     url(r'^api/settings/', include('conf.urls')), # settings API
-    url(r'^api/token-auth/', 'rest_framework_jwt.views.obtain_jwt_token',
+    url(r'^api/token-auth/', obtain_jwt_token,
         name='token-auth'),
-    url(r'^api/token-refresh/', 'rest_framework_jwt.views.refresh_jwt_token',
+    url(r'^api/token-refresh/', refresh_jwt_token,
         name='token-refresh'),
-    url(r'^api/token-verify/', 'rest_framework_jwt.views.verify_jwt_token',
+    url(r'^api/token-verify/', verify_jwt_token,
         name='token-verify'),
     url(r'^api/auth/', include('rest_framework.urls', namespace='rest_framework')),
 
