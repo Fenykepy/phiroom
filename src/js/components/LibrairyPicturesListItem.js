d0ef79b5789ec0d9826dad21c91ea314b09eb80a
Fenykepy <pro@lavilotte-rolle.fr>
2016-01-13 20:54:45 +0100

    beginning of drag & drop ordering.

--- src/js/components/LibrairyPicturesListItem.js
+++ src/js/components/LibrairyPicturesListItem.js
@@ -1,6 +1,7 @@
 import React, { Component, PropTypes } from 'react'
 
 import { PICTURE } from '../constants/dragTypes'
+import { listsHaveCommon } from '../helpers/utils'
 
 import ContextualMenu from './ContextualMenu'
 import LibrairyPicturesListItemMenu from './LibrairyPicturesListItemMenu'
@@ -10,7 +11,12 @@ export default class LibrairyPicturesListItem extends Component {
   constructor(props) {
     super(props)
 
+    this.accepted_drop = [PICTURE]
+
     this.default_state = {
+      baskets: false,
+      padding_right: 0,
+      padding_left: 10,
       contextual_menu: false,
       X: null,
       Y: null
@@ -50,6 +56,33 @@ export default class LibrairyPicturesListItem extends Component {
     return null
   }
 
+  getBaskets() {
+    if (this.state.baskets) {
+      return (
+          <div>
+            <div className="basket-left"
+              onDragEnter={this.handleBasketLeftEnter.bind(this)}
+              onDragLeave={this.handleBasketLeave.bind(this)}
+              style={{
+                width: this.props.columns_width + 'px',
+                right: this.state.padding_right + this.props.columns_width / 2 + 'px'
+              }}
+            />
+            <div className="basket-right"
+              onDragEnter={this.handleBasketRightEnter.bind(this)}
+              onDragLeave={this.handleBasketLeave.bind(this)}
+              style={{
+                width: this.props.columns_width + 'px',
+                left: this.state.padding_left + this.props.columns_width / 2 + 'px'
+              }}
+            />
+          </div>
+      )
+    }
+    return null
+
+  }
+
   handleClick(e) {
     e.stopPropagation() // to avoid unselectAll to trigger
     this.props.handleClick(this.props.index, e.ctrlKey, e.shiftKey)
@@ -77,15 +110,44 @@ export default class LibrairyPicturesListItem extends Component {
     this.props.handleDrag(this.props.pk, this.props.selected)
   }
 
+  handleWrapperDragEnter(e) {
+    if (listsHaveCommon(e.dataTransfer.types, this.accepted_drop)) {
+      e.preventDefault()
+      this.setState({baskets: true})
+    }
+  }
+
+  handleBasketLeftEnter(e) {
+    e.preventDefault()
+    this.setState({padding_left: 100 + 10})
+  }
+
+  handleBasketRightEnter(e) {
+    e.preventDefault()
+    this.setState({padding_right: 100})
+  }
+
+  handleBasketLeave(e) {
+    e.preventDefault()
+      this.setState({
+        baskets: false,
+        padding_right: 0,
+        padding_left: 10,
+      })
+  }
+
   render() {
     return (
       <div className="thumb-wrapper"
         style={{
           height: this.props.columns_width + 'px',
           width: this.props.columns_width + 'px',
-          lineHeight: this.props.columns_width + 'px'
+          lineHeight: this.props.columns_width + 'px',
+          paddingRight: this.state.padding_right + 'px',
+          paddingLeft: this.state.padding_left + 'px',
         }}
         onClick={this.handleWrapperClick.bind(this)}
+        onDragEnter={this.handleWrapperDragEnter.bind(this)}
       >
         <article className={this.props.selected ? 'selected' : null}>
           <img
@@ -98,6 +160,7 @@ export default class LibrairyPicturesListItem extends Component {
           />
         </article>
         {this.getContextualMenu()}
+        {this.getBaskets()}
       </div>
     )
   }
