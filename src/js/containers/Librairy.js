eccb7cc81582ce91fec254688e6750513785006c
Fenykepy <pro@lavilotte-rolle.fr>
2016-02-02 20:47:31 +0100

    start refactoring of librairy.

--- src/js/containers/Librairy.js
+++ src/js/containers/Librairy.js
@@ -1,38 +1,31 @@
 import React, { Component, PropTypes } from 'react'
 
+import { connect } from 'react-redux'
+
 import { Link } from 'react-router'
-import LibrairyLeftPanel from './LibrairyLeftPanel'
+import LibrairyLeftPanel from '../components/LibrairyLeftPanel'
 
 import { setModule } from '../actions/modules'
 import { dragEnd } from '../actions/librairy'
 import { fetchPortfoliosHeadersIfNeeded } from '../actions/portfolios'
 
-import { fetchCSRFTokenIfNeeded } from '../actions/common'
 
-
-export default class Librairy extends Component {
+class Librairy extends Component {
 
   static fetchData(dispatch, params=null, clientSide=false) {
     let promises = []
-    // fetch csrf token
-    promises.push(dispatch(fetchCSRFTokenIfNeeded()))
     // fetch portfolios headers
     dispatch(fetchPortfoliosHeadersIfNeeded())
     // fetch posts headers if user is weblog_author
     // fetch collections headers
-    if (! clientSide) {
-      // set module
-      dispatch(setModule('librairy'))
-    }
+    // set module
+    dispatch(setModule('librairy'))
+    
     return promises
   }
 
   componentDidMount() {
     this.constructor.fetchData(this.props.dispatch, null, true)
-    // set module
-    if (this.props.modules.current != 'librairy') {
-      this.props.dispatch(setModule('librairy'))
-    }
 
     // listen for dragEnd events
     document.addEventListener('dragend', this.handleDragEnd.bind(this))
@@ -46,24 +39,19 @@ export default class Librairy extends Component {
     this.props.dispatch(dragEnd())
   }
 
-  getChildren() {
-    if (this.props.children) {
-      return React.cloneElement(this.props.children, {
-        dispatch: this.props.dispatch,
-        viewport: this.props.viewport,
-        librairy: this.props.librairy,
-      })
-    }
-    return null
-  }
 
   render() {
+    // injected by connect call:
+    const { dispatch } = this.props
     //console.log('lib', this.props)
     return (
         <section role="main">
-          <LibrairyLeftPanel {...this.props}/>
-          {this.getChildren()}
+          <LibrairyLeftPanel />
+          {this.props.children}
         </section>
     )
   }
 }
+
+
+export default connect()(Librairy)
