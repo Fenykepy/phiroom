efbdfd905e2267a88bc547f854ca47f6fe50d9c1
Fenykepy <pro@lavilotte-rolle.fr>
2017-10-08 09:48:46 +0200

    Deactivate weblog component when weblog option is disabled.

--- src/js/containers/Weblog.js
+++ src/js/containers/Weblog.js
@@ -5,6 +5,10 @@ import { connect } from 'react-redux'
 import { setModule } from '../actions/modules'
 import { setDocumentTitleIfNeeded } from '../actions/common'
 
+import { settingsSelector } from '../selectors/settingsSelector'
+
+import ErrorPage from '../components/ErrorPage'
+
 class Weblog extends Component {
 
   static fetchData(dispatch, params, clientSide=false) {
@@ -22,10 +26,24 @@ class Weblog extends Component {
 
   render() {
     // injected by connect() call:
-    const { dispatch } = this.props
+    const {
+      dispatch,
+      activate_weblog,
+    } = this.props
+
+    if (! this.props.activate_weblog) {
+      // if weblog is deactivated, we send 404
+      return (
+        <ErrorPage
+          status={404}
+        />
+      )
+      
+    }
+
 
     return this.props.children
   }
 }
 
-export default connect()(Weblog)
+export default connect(settingsSelector)(Weblog)
